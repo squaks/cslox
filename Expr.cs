6b6559053ade931b9897874972aa0607971c3d32
squaks <devPierce@proton.me>
2024-04-21 17:05:42 -0400

    (5.2) Implementing Syntax Trees

---
+++ Expr.cs
@@ -0,0 +1,48 @@
+﻿namespace cslox
+{
+	internal abstract class Expr
+	{
+		public class Binary : Expr
+		{
+			readonly Expr left;
+			readonly Token op;
+			readonly Expr right;
+
+			public Binary(Expr left, Token op, Expr right)
+			{
+				this.left = left;
+				this.op = op;
+				this.right = right;
+			}
+		}
+		public class Grouping : Expr
+		{
+			readonly Expr expression;
+
+			public Grouping(Expr expression)
+			{
+				this.expression = expression;
+			}
+		}
+		public class Literal : Expr
+		{
+			readonly object value;
+
+			public Literal(object value)
+			{
+				this.value = value;
+			}
+		}
+		public class Unary : Expr
+		{
+			readonly Token op;
+			readonly Expr right;
+
+			public Unary(Token op, Expr right)
+			{
+				this.op = op;
+				this.right = right;
+			}
+		}
+	}
+}
