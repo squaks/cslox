671212f5b6ee45f9e0aaebb8033a6af9f8cfbadd
squaks <devPierce@proton.me>
2024-04-22 22:58:14 -0400

    (7.2) Evaluating Expressions

---
+++ Interpreter.cs
@@ -0,0 +1,94 @@
+﻿namespace cslox
+{
+    internal class Interpreter : Expr.IAstVisitor<object>
+    {
+        public object visitLiteralExpr(Expr.Literal expr)
+        {
+            return expr.value;
+        }
+
+        public object visitUnaryExpr(Expr.Unary expr)
+        {
+            object right = evaluate(expr.right);
+
+            switch (expr.op.type)
+            {
+                case TokenType.BANG:
+                    return !isTruthy(right);
+                case TokenType.MINUS:
+                    return -(double)right;
+            }
+
+            // Unreachable.
+            return null;
+        }
+
+        public bool isTruthy(object obj)
+        {
+            if (obj is null) return false;
+            if (obj is bool) return (bool)obj;
+            return true;
+        }
+
+        public bool isEqual(object a, object b)
+        {
+            if (a is null && b is null) return true;
+            if (a is null) return false;
+
+            return a.Equals(b);
+        }
+
+        public object visitGroupingExpr(Expr.Grouping expr)
+        {
+            return evaluate(expr.expression);
+        }
+
+        public object evaluate(Expr expr)
+        {
+            return expr.accept(this);
+        }
+
+        public object visitBinaryExpr(Expr.Binary expr)
+        {
+            object left = evaluate(expr.left);
+            object right = evaluate(expr.right);
+
+            switch (expr.op.type)
+            {
+                case TokenType.BANG_EQUAL:
+                    return !isEqual(left, right);
+                case TokenType.EQUAL_EQUAL:
+                    return isEqual(left, right);
+                case TokenType.GREATER:
+                    return (double)left > (double)right;
+                case TokenType.GREATER_EQUAL:
+                    return (double)left >= (double)right;
+                case TokenType.LESS:
+                    return (double)left < (double)right;
+                case TokenType.LESS_EQUAL:
+                    return (double)left <= (double)right;
+                case TokenType.MINUS:
+                    return (double)left - (double)right;
+                case TokenType.PLUS:
+                    if (left is double && right is double)
+                    {
+                        return (double)left + (double)right;
+                    }
+
+                    if (left is string && right is string)
+                    {
+                        return (string)left + (string)right;
+                    }
+
+                    break;
+                case TokenType.SLASH:
+                    return (double)left / (double)right;
+                case TokenType.STAR:
+                    return (double)left * (double)right;
+            }
+
+            // Unreachable
+            return null;
+        }
+    }
+}
